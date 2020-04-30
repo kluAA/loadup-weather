ea2c1fe47a8c21f435cc1b9de771d8451100f58b
kevinlu <kevinlu.rea@gmail.com>
2020-04-30 17:12:11 -0400

    Add weather condition and temperature

--- frontend/src/components/weather/weather.jsx
+++ frontend/src/components/weather/weather.jsx
@@ -13,7 +13,7 @@ class Weather extends React.Component {
 
     getWeather() {
         if (navigator.geolocation) {
-            this.setState({ showButton: false, showWeather: true, isLoading: true})
+            this.setState({ showButton: false, showWeather: false, isLoading: true})
             // grabs lat, lon coords
             navigator.geolocation.getCurrentPosition(position => {
                 const lat = position.coords.latitude;
@@ -21,7 +21,7 @@ class Weather extends React.Component {
 
                 //api request to fetch current weather data
                 this.props.fetchWeatherByCoords(lat, lon)
-                    .then(weather => this.setState({isLoading: false}));
+                    .then(weather => this.setState({isLoading: false, showWeather: true}));
             });
         } 
         else {
@@ -29,9 +29,31 @@ class Weather extends React.Component {
         }
     }
 
+    weatherInfo() {
+        const { currentWeather } = this.props;
+        if (!currentWeather) return null;
+        return (
+            <div className="weather-info">
+                {/* <h1 className="city">
+                    {currentWeather.name}
+                </h1> */}
+                <div className="weather-header">
+                    <div className="condition">
+                        <img className="weather-icon" src={`http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`}></img>
+                        <span>{currentWeather.weather[0].main}</span>
+                    </div>
+                    <h1 className="temperature">{Math.round(currentWeather.main.temp) + "°F"}</h1>
+          
+
+                </div>
+            </div>
+
+        )
+    }
+
     render() {
         const { showButton, showWeather, isLoading } = this.state;
-        
+        const { currentWeather } = this.props;
         const currentWeatherBtn = (
             <button className="weather-btn" onClick={this.getWeather}>
                 <i className="fas fa-cloud-sun-rain"></i> Current Weather
@@ -48,9 +70,12 @@ class Weather extends React.Component {
         );
  
         return (
-            <div className="weather-container">
+            <div className="weather-container"
+                id={currentWeather.weather ? "raining" : null}
+            >
                 { showButton && currentWeatherBtn }
                 { isLoading && loading }
+                { showWeather && this.weatherInfo()}
             </div>
         );
     }
