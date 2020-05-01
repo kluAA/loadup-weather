655190bf04e9249e26a02876d64e220bc5316567
kevinlu <kevinlu.rea@gmail.com>
2020-05-01 08:28:06 -0400

    Style navbar

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from "./logo.png";
 
 class NavBar extends React.Component {
     constructor(props) {
@@ -9,10 +10,19 @@ class NavBar extends React.Component {
         return (
             <div className="nav-wrapper">
                 <nav className="nav-top">
-
+                    <p className="nt-left">
+                        Nationwide Junk Removal
+                        <span className="sep">|</span>
+                        <a href="http://goloadup.com/cities"><i className="fas fa-map-marker-alt"></i> Find My City <i className="fas fa-chevron-right"></i></a>
+                    </p>
                 </nav>
                 <nav className="nav-bottom">
-
+                    <div className="logo-container">
+                        <img className="logo" src={logo}></img>
+                    </div>
+                    <div className="challenge">
+                        <h1>Weather App Challenge</h1>
+                    </div>
                 </nav>
             </div>
         )
